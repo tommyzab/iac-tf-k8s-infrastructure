8cc8a62eb5bbdcd29a9d566716766af99b375965
Tommy <tommyzabari@gmail.com>
2026-03-09 17:31:59 -0500

    refactor: update kubernetes and helm providers to use exec plugins for secure EKS auth

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -14,5 +14,5 @@ variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
   type        = string
   default     = "terraform-state-locking-v1"
-} 
+}
  
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -37,7 +37,11 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
+  }
 }
 
 # Configure helm provider for deploying Kubernetes applications
@@ -45,10 +49,13 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
   }
 }
-
 # ==============================================================================
 # GITHUB ACTIONS OIDC AUTHENTICATION
 # ==============================================================================
